9d1726b51ea1ff2109e363a48db041b6cfc4a5c5
Ruslan Asenov <Ruslan.Asenov@motorolasolutions.com>
2021-06-01 16:16:06 +0200

    Electron impl.

---
+++ www/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+    const win = new BrowserWindow({
+      width: 1024,
+      height: 768,
+      webPreferences: {
+        
+      }
+    })
+  
+    win.loadFile('index.html');
+    win.webContents.openDevTools();
+  }
+
+  app.whenReady().then(() => {
+    createWindow()
+  })
+
+  app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+  })
