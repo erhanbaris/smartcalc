46bf4cb75abe98c89a0a42e97285939a7620531b
Erhan BARIS <erhanbaris@gmail.com>
2021-08-15 21:22:39 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,40 @@ extern crate libsmartcalc;
 use libsmartcalc::executer::{initialize};
 use libsmartcalc::app::SmartCalc;
 
+#[cfg(feature="webserver")]
 mod webserver;
 
+#[cfg(feature="webserver")]
 fn main() {
-    if cfg!(feature="webserver") {
-        tokio::runtime::Runtime::new().unwrap().block_on(async {
-            match webserver::start_webserver().await {
-                Ok(_) => println!("Webserver stopped"),
-                Err(error) => println!("Error : {:}", error),
-            };
-        });
-    } else {
-        let test_data = r"
-        date information = 11:30
-        date information add 1 hour 1 minute 31 second".to_string();
-        initialize();
+    tokio::runtime::Runtime::new().unwrap().block_on(async {
+        match webserver::start_webserver().await {
+            Ok(_) => println!("Webserver stopped"),
+            Err(error) => println!("Error : {:}", error),
+        };
+    });
+}
+
+#[cfg(not(feature="webserver"))]
+fn main() {
+    let test_data = r"
+    date information = 11:30
+    date information add 1 hour 1 minute 31 second".to_string();
+    initialize();
+
+    let app = SmartCalc::default();
+    let language = "en".to_string();
+    let results = app.execute(&language, &test_data);
     
-        let app = SmartCalc::default();
-        let language = "en".to_string();
-        let results = app.execute(&language, &test_data);
-        
-        for result in results.lines.iter() {
-            match result {
-                Some(result) => match result {
-                    Ok(line) => {
-                        println!("{:?}", line.tokens);
-                        println!("{}", line.output)
-                    },
-                    Err(error) => println!("Error : {}", error)
+    for result in results.lines.iter() {
+        match result {
+            Some(result) => match result {
+                Ok(line) => {
+                    println!("{:?}", line.tokens);
+                    println!("{}", line.output)
                 },
-                None => println!("No query")
-            }
+                Err(error) => println!("Error : {}", error)
+            },
+            None => println!("No query")
         }
     }
 }
